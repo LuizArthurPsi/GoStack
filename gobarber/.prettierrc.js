82c70de438d26b8a8ec862bed93d1b892ddabdfc
Luiz Arthur <luizarthurvm@gmail.com>
2021-04-08 08:32:39 -0300

    Add of ESLint and Prettier

---
+++ gobarber/.prettierrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+	bracketSpacing: false,
+	jsxBracketSameLine: true,
+	singleQuote: true,
+	trailingComma: "all",
+
+	printWidth: 120,
+	"editor.formatOnSave": true,
+	proseWrap: "always",
+	tabWidth: 4,
+	requireConfig: false,
+	useTabs: true,
+	trailingComma: "none",
+	bracketSpacing: true,
+	jsxBracketSameLine: false,
+	semi: true
+};
