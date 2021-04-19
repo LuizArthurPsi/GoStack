37ae8960f1f45c3e3248951e0cdaa9af2b9286dc
Luiz Arthur <luizarthurvm@gmail.com>
2021-04-19 18:22:14 -0300

    Final Att back-end 1

--- gobarber/src/app/controllers/SessionController.js
+++ gobarber/src/app/controllers/SessionController.js
@@ -1,32 +1,40 @@
 import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
 
 import User from '../models/User';
+import authConfig from '../../config/auth';
 
 class SessionController {
-    async store(req, res) {
-        const { email, password } = req.body;
+	async store(req, res) {
+		const schema = Yup.object().shape({
+			email: Yup.string().email().required(),
+			password: Yup.string().required()
+		});
 
-        const user = await User.findOne({ where : {email } });
+		if (!(await schema.isValid(req.body))) {
+			return res.status(400).json({ error: 'Validation fails' });
+		}
+		const { email, password } = req.body;
 
-        if (!user) {
-            return res.status(401).json({ error: "User not found."});
-        }
+		const user = await User.findOne({ where: { email } });
 
-        if (!(await user.checkPassword(password))) {
-            return res.status(401).json({ error: 'Password does not match.'});
-        }
+		if (!user) {
+			return res.status(401).json({ error: 'User not found.' });
+		}
 
-        const { id, name } = user;
+		if (!(await user.checkPassword(password))) {
+			return res.status(401).json({ error: 'Password does not match.' });
+		}
 
-        return res.json({
-            user: {
-                id,
-                name,
-                email,
-            },
-            token: jwt.sign({ id }, 'e950f39f7d7272974d2d64ab9446295b', {
-                expiresIn: '7d',
-            }), 
-        }),
-    }
+		const { id, name } = user;
+
+		return res.json({
+			user: { id, name, email },
+			token: jwt.sign({ id }, authConfig.secret, {
+				expiresIn: authConfig.expiresIn
+			})
+		});
+	}
 }
+
+export default new SessionController();
---
+++ gobarber/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+	secret: 'e950f39f7d7272974d2d64ab9446295b',
+	expiresIn: '7d'
+};
