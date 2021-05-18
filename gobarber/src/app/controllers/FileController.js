5c4cbe0d335aba5ad4f0e943c9482593c8bd7004
Luiz Arthur <luizarthurvm@gmail.com>
2021-05-18 19:03:15 -0300

    Send files

--- gobarber/src/app/controllers/FileController.js
+++ gobarber/src/app/controllers/FileController.js
@@ -1,7 +1,13 @@
 /* eslint-disable class-methods-use-this */
+import File from '../models/File';
+
 class FileController {
 	async store(req, res) {
-		return res.json(req.file);
+		const { originalname: name, filename: path } = req.file;
+
+		const file = await File.create({ name, path });
+
+		return res.json(file);
 	}
 }
 
