d97bc8613a71089119dd66ac632c2c30d419bfd9
Luiz Arthur <luizarthurvm@gmail.com>
2021-05-31 15:05:33 -0300

    Feat: Configurando o nodemail

---
+++ gobarber/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+	host: 'smtp.mailtrap.io',
+	port: 2525,
+	secure: false,
+	auth: {
+		user: 'c8469b4faee7b5',
+		pass: '404497bafee527'
+	},
+	default: {
+		from: 'Equipe GoBarber <noreply@gobarber.com'
+	}
+};
